d6b5ee093a37429e71eccff2774c50e420373254
Nicola <nicolusso@gmail.com>
2021-01-31 22:07:15 +0100

    finish

--- js/script.js
+++ js/script.js
@@ -8,11 +8,30 @@ var app = new Vue({
       'Lavare i piatti',
       'Portare al parco il cane'
     ],
+
+    todosDelete : [],
   },
   methods : {
+    // Aggiungere alla Lista
     addTodo(){
       this.todos.push(this.newTodo);
       this.newTodo = '';
-    }
+    },
+
+    // Rimuovere dalla Lista
+    deleteTodo(index){
+      this.todosDelete.push(this.todos[index]);
+      this.todos.splice(index, 1);
+    },
+    restoreTodo(index){
+      this.todos.push(this.todosDelete[index]);
+      this.todosDelete.splice(index,1);
+   },
+    confirmDelete(index){
+      this.todosDelete.splice(index,1);
+   },
+    deleteAll(){
+      this.todosDelete.splice(0,this.todosDelete.length);
+   }
   },
 })
